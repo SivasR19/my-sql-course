b2dd167ddaa8651adbe0b1bca6cc21d395aae774
SivasR19 <Rajean.Sivagnaneswaran@uhcw.nhs.uk>
2025-07-17 15:02:52 +0000

    finished afternoon sesh

--- case example.sql
+++ case example.sql
@@ -181,3 +181,25 @@ SELECT
 FROM p
 GROUP BY p.PatientTariffGroup
 ORDER BY p.PatientTariffGroup;
+
+-- temp tables
+drop table if exists #TempPatientStay
+
+SELECT
+    ps.PatientId
+    ,ps.AdmittedDate
+    ,ps.Tariff  
+    ,CASE WHEN ps.Tariff >= 7 THEN 'High Tariff'
+        WHEN ps.Tariff >= 4 THEN 'Medium Tariff'
+        ELSE 'Low Tariff'
+    END AS TariffGroup
+INTO #TempPatientStay
+FROM
+    dbo.PatientStay ps
+
+    SELECT
+    p.TariffGroup
+    , COUNT(*) AS NumberOfPatients      
+FROM #TempPatientStay as p
+GROUP BY p.TariffGroup
+ORDER BY p.TariffGroup;
